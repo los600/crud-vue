cbb8932718751c0cebe12d4479bebc7048795912
Davina-dev <davinamr91@gmail.com>
2021-11-24 12:21:34 +0100

    add array

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const app = new Vue({                          //instancia un nuevo objeto de vue
     el: '#app',                               // el --> atributo (elemento) y hace referencia al iddel elemento raiz que queremos conectar en html
     data:{                                    //obteto
-        saludar: ' ♥ ♡ .Hola Compis. ♡ ♥ '     //contenido del objeto data   // progresivo -->  app.saludar='☘ adiós compis ☘'
-        } 
+        saludar: ' ♥ ♡ .Hola Compis. ♡ ♥ ',   //contenido del objeto data   // progresivo -->  app.saludar='☘ adiós compis ☘'
+        newContacts: [
+            {
+                name: 'Davina',
+                mail: 'davinamr91@gmail.com',
+            },
+            {
+                name: 'Davina',
+                mail: 'davinamr91@gmail.com',
+            }
+        ]
+    },
+
+    
     });
-        
