064530de3c30b1ad315d731aefb155a0ebe561c5
Davina-dev <davinamr91@gmail.com>
2021-11-24 11:49:28 +0100

    h

---
+++ main.js
@@ -0,0 +1,7 @@
+const app = new Vue({                          //instancia un nuevo objeto de vue
+    el: '#app',                               // el --> atributo (elemento) y hace referencia al iddel elemento raiz que queremos conectar en html
+    data:{                                    //obteto
+        saludar: ' ♥ ♡ .Hola Compis. ♡ ♥ '     //contenido del objeto data   // progresivo -->  app.saludar='☘ adiós compis ☘'
+        } 
+    });
+        
